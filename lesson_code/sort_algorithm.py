2db94ac99f823ed5ec746d13d3b552d4874da371
ChingPiWu <ginabb5577bb36@gmail.com>
2024-12-17 19:20:23 +0800

    [refator]

--- lesson_code/sort_algorithm.py
+++ lesson_code/sort_algorithm.py
@@ -1,34 +1,36 @@
 from .math_problem import *
 
-def bubble_sort(nums:list):
-    ##Bubble Sort
-  sum= 0
-  for j in range(len(nums)-1):
-    for i in range(len(nums)-1):
-      if nums[i]>nums[i+1]:
-        nums[i],nums[i+1] = nums[i+1],nums[i]
-      else:
-        continue
-    sum+=1
-    print(sum,nums)
 
-  return nums
+def bubble_sort(nums: list):
+    # Bubble Sort
+    sum = 0
+    for j in range(len(nums)-1):
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+            else:
+                continue
+        sum += 1
+        print(sum, nums)
+
+    return nums
 
 
-def selection_sort(nums:list):
-    ##Selection sort
-  min_i =0
-  n = len(nums)
-  for j in range(n-1):
-    min_i =j
-    for i in range(j+1,n):
-      if nums[i]<nums[min_i]:
-        min_i = i
+def selection_sort(nums: list):
+    # Selection sort
+    min_i = 0
+    n = len(nums)
+    for j in range(n-1):
+        min_i = j
+        for i in range(j+1, n):
+            if nums[i] < nums[min_i]:
+                min_i = i
 
-    nums[j],nums[min_i] = nums[min_i],nums[j]
+        nums[j], nums[min_i] = nums[min_i], nums[j]
 
-    print(f'round = {j},min_i = {min_i},nums = {nums}')
-  return nums
+        print(f'round = {j},min_i = {min_i},nums = {nums}')
+    return nums
+
 
 if __name__ == '__main__':
     print("ABC")
@@ -36,4 +38,4 @@ if __name__ == '__main__':
     print("Bubble sort:", bubble_sort(nums))
     print("Selection sort:", selection_sort(nums))
 else:
-  print('ABC: ', __name__)
+    print('ABC: ', __name__)
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import lesson_code
-print(lesson_code.bubble_sort([3,2,1]))
-print(lesson_code.right_angle(3,4))
+print(lesson_code.bubble_sort([3, 2, 1]))
+print(lesson_code.right_angle(3, 4))
